66113deb31f5c7c4f4f9ccd9493b0d395b9f6668
JCSnap <e0970167@u.nus.edu>
2024-02-08 21:07:52 +0800

    add initial throttle implementation

---
+++ src/throttle.ts
@@ -0,0 +1,26 @@
+import { RateLimit } from "./types/rate-limit";
+import { type Connection } from "./types/connection";
+import { PlatformRateLimiter } from "./platform-rate-limiter";
+import { Job } from "./job";
+import { TaskScheduler } from "./task-scheduler";
+
+const platformRateLimiters = new Map<string, PlatformRateLimiter<any, any>>();
+const taskScheduler = new TaskScheduler(platformRateLimiters);
+
+export function throttle<T, U>(connection: Connection, fn: (arg: T) => Promise<U>, arg: T): Promise<U> {
+  let platformRateLimiter = platformRateLimiters.get(connection.platform);
+  if (!platformRateLimiter) {
+    platformRateLimiter = createAndAddPlatformRateLimiter(connection.platform, connection.rateLimit);
+  }
+  const job = new Job(fn, arg, connection.niceness);
+  platformRateLimiter.enqueue(job);
+  return job.getPromise();
+}
+
+taskScheduler.pollForJobs();
+
+function createAndAddPlatformRateLimiter(platform: string, rateLimit: RateLimit): PlatformRateLimiter<any, any> {
+  const platformRateLimiter = new PlatformRateLimiter(platform, rateLimit);
+  platformRateLimiters.set(platform, platformRateLimiter);
+  return platformRateLimiter;
+}
