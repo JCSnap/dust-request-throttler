87567e7638ea05e0e82c9864ddf3c7988a5a96e7
JCSnap <e0970167@u.nus.edu>
2024-02-09 02:57:34 +0800

    add sample functions to constants

--- src/constants.ts
+++ src/constants.ts
@@ -43,3 +43,103 @@ export const SLACK_CONNECTION = {
   niceness: 0,
   rateLimit: SLACK_RATE_LIMIT,
 };
+
+// more connections for simulation
+export const NOTION_CONNECTION_GARBAGE_COLLECTOR = {
+  platform: "notion",
+  connection: "notion_connection_garbage_collector",
+  niceness: 5, // not important
+  rateLimit: NOTION_RATE_LIMIT,
+};
+
+export const NOTION_CONNECTION_BACKUP = {
+  platform: "notion",
+  connection: "notion_connection_backup",
+  niceness: 3,
+  rateLimit: NOTION_RATE_LIMIT,
+};
+
+export const NOTTION_CONNECTION_ANALYTICS = {
+  platform: "notion",
+  connection: "notion_connection_analytics",
+  niceness: 1,
+  rateLimit: NOTION_RATE_LIMIT,
+};
+
+export const GOOGLE_DRIVE_CONNECTION_BACKUP = {
+  platform: "google_drive",
+  connection: "google_drive_connection_backup",
+  niceness: 3,
+  rateLimit: GOOGLE_DRIVE_RATE_LIMIT,
+};
+
+export const GOOGLE_DRIVE_CONNECTION_ANALYTICS = {
+  platform: "google_drive",
+  connection: "google_drive_connection_analytics",
+  niceness: 1,
+  rateLimit: GOOGLE_DRIVE_RATE_LIMIT,
+};
+
+export const SLACK_CONNECTION_BACKUP = {
+  platform: "slack",
+  connection: "slack_connection_backup",
+  niceness: 3,
+  rateLimit: SLACK_RATE_LIMIT,
+};
+
+export const SLACK_CONNECTION_ANALYTICS = {
+  platform: "slack",
+  connection: "slack_connection_analytics",
+  niceness: 1,
+  rateLimit: SLACK_RATE_LIMIT,
+};
+
+// functions
+export async function collectGarbage() {
+  await new Promise((resolve) => {
+    console.log("Garbage collecting...");
+    setTimeout(resolve, 3000);
+  });
+}
+
+export async function backup() {
+  await new Promise((resolve) => {
+    console.log("Backing up...");
+    setTimeout(resolve, 500);
+  });
+}
+
+export async function analyse() {
+  await new Promise((resolve) => {
+    console.log("Analyzing...");
+    setTimeout(resolve, 2000);
+  });
+}
+
+export async function sendNotification() {
+  await new Promise((resolve) => {
+    console.log("Sending notification...");
+    setTimeout(resolve, 1000);
+  });
+}
+
+export async function collectData() {
+  await new Promise((resolve) => {
+    console.log("Collecting data...");
+    setTimeout(resolve, 4000);
+  });
+}
+
+export async function processData() {
+  await new Promise((resolve) => {
+    console.log("Processing data...");
+    setTimeout(resolve, 6000);
+  });
+}
+
+export async function sendData() {
+  await new Promise((resolve) => {
+    console.log("Sending data...");
+    setTimeout(resolve, 5000);
+  });
+}
